03fe472709a7e931aa95eabdb84711aea6b2408a
Imir0nMan <sornamen315@gmail.com>
2025-03-03 19:58:13 +0400

    made it generic

--- constants.py
+++ constants.py
@@ -1,9 +1,9 @@
 #bot token
-TOKEN = "7551368262:AAFUXphEpODYwg2AbLx9ZZKNNveKEPcLmAQ"
+TOKEN = "Az1ghTsb:2456Qkjsh"	#Example
 
 #id of user who can get user data in xlsx
-THE_ID = 1271736470
+THE_ID = 123456789				#Example
 
 #id of channel from where bot gets new mailing messages
-CHANNEL_ID = -1002332631342
+CHANNEL_ID = -1001234567879		#Example
 
